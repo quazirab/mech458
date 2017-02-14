42cfbf7f5717f99cf121559715fdd80210ff475f
McM-Ev <mcmillan.ev@gmail.com>
2017-02-13 22:43:58 -0800

    Combined elemExt() and display functions using a loop
    -Added the LinkedQueue files from the coursespaces website
    -Combined the two functions elemExt() and display() into a single
    function using a while() loop. Allows any number of nodes to be handled.

--- lab3/LinkedQueue/LinkedQueue.c
+++ lab3/LinkedQueue/LinkedQueue.c
@@ -20,178 +20,87 @@
 #include <stdlib.h>
 #include <avr/io.h>
 #include "LinkedQueue.h" 	/* This is the attached header file, which cleans things up */
-#include <util/delay_basic.h>
-#include<avr/interrupt.h>
-
-/* Make sure you read it!!! */
+							/* Make sure you read it!!! */
 /* global variables */
 /* Avoid using these */
 
 /* main routine */
-	volatile link *head;			/* The ptr to the head of the queue */
-	volatile link *tail;			/* The ptr to the tail of the queue */
-	volatile link *newLink;			/* A ptr to a link aggregate data type (struct) */
-	volatile link *rtnLink;			/* same as the above */
-	volatile int state = 1;			/*WHEN == 1, LED WILL START TOP DISPLAY */
-	volatile element e1 = null;		/* A variable to hold the aggregate data type known as element */
-	volatile element e2 = null;		/* A variable to hold the aggregate data type known as element */
-	volatile element e3 = null;		/* A variable to hold the aggregate data type known as element */
-
-	void debug();
-	void elemExt();
-	void dislay();
-
-		
 int main(){	
 
-	
-	//element eTest;		/* A variable to hold the aggregate data type known as element */
+	link *head;			/* The ptr to the head of the queue */
+	link *tail;			/* The ptr to the tail of the queue */
+	link *newLink;		/* A ptr to a link aggregate data type (struct) */
+	link *rtnLink;		/* same as the above */
+	element eTest;		/* A variable to hold the aggregate data type known as element */
 
-	DDRA = 0x00; //SET ALL OF THE PORT A TO INPUT BITS
-  	DDRB = 0xFF; //SET ALL OF THE PORT B TO OUTPUT BITS
+	DDRB = 0xFF; 		/* Used for debugging purposes only */
 				
+
 	rtnLink = NULL;
 	newLink = NULL;
 
 	setup(&head, &tail);
 
-	while(1){
-  		while((PINA & 0x04) == 0x04);
-				timerCount(20);
-		while((PINA & 0x04) == 0x00);
-				timerCount(20);
-				debug();
+	/* 
+		Many of the following lines will test to see if your algorithms will work. You do not neccessarily
+		need the MCU attached to the computer to test this, and can do most of the work using the 
+		debugger in the AVR studio while observing the I/O View in the top right corner. Click the tab
+		or PORTB to see how the output chages while 'stepping' through the routines.
+	*/
+	/* Initialize a new link here */
+	initLink(&newLink);
+	newLink->e.itemCode = 3;
+	newLink->e.stage = 4;
+	enqueue(&head, &tail, &newLink);
+	PORTB = head->e.itemCode;
+	PORTB = tail->e.stage;
+
+	initLink(&newLink);
+	newLink->e.itemCode = 5;
+	newLink->e.stage = 6;
+	enqueue(&head, &tail, &newLink);
+	PORTB = head->e.itemCode;
+	PORTB = tail->e.stage;
+
+	initLink(&newLink);
+	newLink->e.itemCode = 7;
+	newLink->e.stage = 8;
+	enqueue(&head, &tail, &newLink);
+	PORTB = head->e.itemCode;
+	PORTB = tail->e.stage;
+
+	PORTB = 0x00;
+
+	/* Tests to see if firstValue works */
+	eTest = firstValue(&head);
+	PORTB = eTest.itemCode;
+	PORTB = 0x00;
+
+	/* Tests if Size works */
+	PORTB = size(&head, &tail);
+
+	/* Tests if dequeue works - Uncomment to use*/
+	//dequeue(&head, &rtnLink); /* remove the item at the head of the list */
+	//PORTB = rtnLink->e.itemCode;
+	//dequeue(&head, &rtnLink); /* remove the item at the head of the list */
+	//PORTB = rtnLink->e.itemCode;
+	//dequeue(&head, &rtnLink); /* remove the item at the head of the list */
+	//PORTB = rtnLink->e.itemCode;
 	
-	
-	}//while
-	
+	/* Tests is empty */
+	PORTB = isEmpty(&head);
+
+	/* Tests to see if clearQueue works*/
+	clearQueue(&head, &tail);
+	PORTB = size(&head, &tail);
+
+	PORTB = isEmpty(&head);
+
+
 	return(0);
 }/* main */
-		
-	
-void debug(){
-
-	initLink(&newLink); 
-	
-	readInput = (PINA & 0x03); //ONLY THE INPUT BITS ARE READ
-	switch (state){
-		case (1):				
-		//Input1 = readInput;
-		state++;							//increment the state variable to indicate a the button has been pressed
-		break;
-		
-		case (2):
-		newLink->e.itemCode = readInput;
-		enqueue(&head, &tail, &newLink); 
-		state++;
-		break;
-		
-		case (3):
-		newLink->e.itemCode = readInput;
-		enqueue(&head, &tail, &newLink); 
-		state++;
-		break;
-		
-		case (4):
-		newLink->e.itemCode = readInput;
-		enqueue(&head, &tail, &newLink); 
-		/*
-		elemExt();		//extracts all the data
-		dislay();		//displays it
-		*/
-		elemDisp();
-		state++;
-		break;
-
-		case (5):
-		PORTC = 0;	
-		state =1;
-		break;
-}//debug
-
-/*As long as the list is not NULL, this function will extract and display the head of the list. Dequeue also frees the head of the list. */
-void elemDisp(){
-	element disp;
-	int shiftCount = 0;						//Declare shiftCount to shift the bits that PORTC will display on.
-	while(*head != NULL){					//While the list is not empty, loop.
-		dequeue(&head, &rtnLink);			//Extract the head of the list and set it to 'rtnLink'
-		disp = rtnLink->e.itemCode;			//Set the value of the head of the lists 'itemCode' variable to the 'disp' element
-		PORTC |= (disp << shiftCount);		//Set PORTC to display the value in 'disp' according to how many bits are being shifted, while keeping any previously displayed results
-		timercount(2000);					
-		free(rtnLink);						//Free the memory occupied by 'rtnLink'
-		disp = NULL;						//set 'disp' to a NULL value
-		shiftCount += 2;					//Increment shiftCount by 2, causing the display line above to shift the output by 2 bits to the left on the next loop.
-	}
-}
-
-/*	Commented out - combined elemExt() and display() into elemDisp(), making use of a single while loop.
-void elemExt(){
-						
-		dequeue(&head, &rtnLink);	//dequeues the first value
-		e1=rtnLink->e.itemCode;		//assigns the first value to the e1
-		free(rtnLink);			//free
-		e2=rtnLink->e.itemCode;
-		free(rtnLink);			//free
-		e3=rtnLink->e.itemCode;
-		free(rtnLink);			//free
-		return 0;
-}//element
-	
-void dislay(){
-	//display algorithm
-	PORTC = e1;
-	timerCount(2000);
-	PORTC |= (e2<<2);
-	timerCount(2000);
-	PORTC |= (e3<<4);
-	timerCount(2000);
 
 
-	e1=NULL;
-	e2=NULL;
-	e3=NULL;
-}//display
-	
-*/
-
-		
-void timerCount(int tim){
-	
-	int i = 0;
-	
-	//Set Presaler TO 1
-	TCCR1B = (1<<CS10);
-	
-	//Set timer clear on Comparision CTC
-	TCCR1B |= (1<<WGM12);
-	
-	
-	
-	//Comparison Register to 1000 cycles for 1 ms
-	OCR1A = 0x03e8;		
-	
-	//Set inital Value of the timer Counter to 0x0000
-	TCNT1 = 0x0000;
-	
-	//Enable the output compare interrut enable
-	TIMSK1 = TIMSK1|0x02;
-	
-	//Clear the timer interrupt flag and begin timer
-	TIFR1 = (1<<OCF1A);
-	
-	//Poll the timer to determine when the timer has reached 1ms
-	//Timer is set for 1 ms, so it will be in while for tim X 1ms.
-	while(i<tim){
-		if((TIFR1 & 0x02)==0x02){  // sees if the 0CF1A flag is up 
-			
-			TIFR1 =(1<<OCF1A);
-			i++;
-		}
-	}
-	return;
-}		
-		
-		
 /**************************************************************************************/
 /***************************** SUBROUTINES ********************************************/
 /**************************************************************************************/
@@ -265,12 +174,9 @@ void enqueue(link **h, link **t, link **nL){
 void dequeue(link **h, link **deQueuedLink){
 	/* ENTER YOUR CODE HERE */
 	*deQueuedLink = *h;	// Will set to NULL if Head points to NULL
-	link *temp;
 	/* Ensure it is not an empty queue */
 	if (*h != NULL){
-		*temp = *h;			//Set the *h pointer to a temporary pointer, so as not to lose it
-		*h = (*h)->next;	//Set *h to be the next node in the linked list
-		free(temp);			//free the memory occupied by the previous *h node
+		*h = (*h)->next;
 	}/*if*/
 	
 	return;
