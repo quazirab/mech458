2e1ad55edbf50c604ef35a19618d2c8bcd71b2c2
McM-Ev <mcmillan.ev@gmail.com>
2017-02-28 15:53:13 -0800

    Lab Progress - Not Working
    need to troubleshoot stuff. Changed to PORT F1, bits aren't changing.

--- lab4/lab4_ADC.c
+++ lab4/lab4_ADC.c
@@ -1,4 +1,3 @@
-/* Solution Set for the LinkedQueue.c */
 /* 	
 	Course		: UVic Mechatronics 458
 	Milestone	: 3
@@ -18,7 +17,13 @@
 #include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay_basic.h>
-#include<avr/interrupt.h>
+#include <avr/interrupt.h>
+
+void setupADC();
+void startConversion();
+//void debug();
+void timerCount(int tim);
+
 
 /* Make sure you read it!!! */
 /* global variables */
@@ -29,15 +34,28 @@
 //USE PC5 as pot input
 
 volatile double dutyCycle=0;
+volatile int ADC_result_flag = 0;
 
 int main(){
+	 cli();
+	DDRC = 0xFF;;	//SET ALL OF THE PORT C TO OUTPUT BITS
+	DDRD = 0xFF;	//SET ALL OF THE PORT D TO OUTPUT BITS
+	DDRF = 0x00;	//SET ALL OF PORT F TO INPUT BITS
 
-	DDRC = 0x00; //SET ALL OF THE PORT A TO INPUT BITS
-  DDRD = 0xFF; //SET ALL OF THE PORT D TO OUTPUT BITS
-
-  debug();	
+	//debug();
+	setupADC();	
+	
+	sei();
+	startConversion();
+	PORTC = 0xff;
+	
 	while(1){
-    
+		
+		if(ADC_result_flag){
+			PORTC = dutyCycle;
+			ADC_result_flag = 0;
+		}//if
+		
 	}//while
 	
 	return(0);
@@ -45,69 +63,20 @@ int main(){
 		
     
 void setupADC(){
-  ADMUX = (1<< REFS0)|(1<<MUX0)|(1<<MUX2);
-  ADCSRA=(1<<ADEN)|(1<<ADIE)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
+  
+  ADMUX = (1<< REFS0)|(1<<MUX0)|(1<<ADLAR);											// USE EXTERNAL POWER SOURCE
+  ADCSRA=(1<<ADEN)|(1<<ADIE)/*|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2)*/;				//
   DIDR0 = (1<<ADC5D);
   
-  startConversion();
+  
 }
 
 void startConversion(){
-  ADCSRA |= (1<<ADSC);
-
+	ADCSRA |= (1<<ADSC);
 }
 
 ISR(ADC_vect){
   dutyCycle = ADC;
+  ADC_result_flag = 1;
   startConversion();
 }
-void debug(){
-	
-		TCCR0A =(1<<COM0A1) | (1<<WGM00) | (1<<WGM01);    //fast PWM
-    TIMSK0=(1<<TOIE0);    //set to overflow
-  
-    sei();
-		TCCR0B = (1<<CS00);
-		
-}//debug
-	
-ISR(TIMER0_OVF_vect){
-    OCR0A = dutyCycle;
-}//isr
-
-		
-void timerCount(int tim){
-	
-	int i = 0;
-	
-	//Set Presaler TO 1
-	TCCR1B = (1<<CS10);
-	
-	//Set timer clear on Comparision CTC
-	TCCR1B |= (1<<WGM12);
-	
-	
-	
-	//Comparison Register to 1000 cycles for 1 ms
-	OCR1A = 0x03e8;		
-	
-	//Set inital Value of the timer Counter to 0x0000
-	TCNT1 = 0x0000;
-	
-	//Enable the output compare interrut enable
-	TIMSK1 = TIMSK1|0x02;
-	
-	//Clear the timer interrupt flag and begin timer
-	TIFR1 = (1<<OCF1A);
-	
-	//Poll the timer to determine when the timer has reached 1ms
-	//Timer is set for 1 ms, so it will be in while for tim X 1ms.
-	while(i<tim){
-		if((TIFR1 & 0x02)==0x02){  // sees if the 0CF1A flag is up 
-			
-			TIFR1 =(1<<OCF1A);
-			i++;
-		}//if
-	}//whil
-	return;
-}//timer count	
