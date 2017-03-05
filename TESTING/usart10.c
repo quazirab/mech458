23617ca9c6d2fc4925fd281c45ba0bd2d04cb73b
quazirab <quazi.rabbi@gmail.com>
2017-03-05 15:52:18 -0800

    New update with timer

--- TESTING/usart10.c
+++ TESTING/usart10.c
@@ -7,6 +7,8 @@
 #define BAUD  9600
 #define BRC ((1000000/16/BAUD)-1)
 
+void timerCount(int tim);
+
 //PD3 = TX
 
 int main(){	
@@ -19,6 +21,46 @@ int main(){
   
   while(1){
     UDR0 = '8';
+    timerCount(1000);
   }
   
 
+  /**************************************************************************************/
+/***************************** Timer***************************************************/
+/**************************************************************************************/
+
+void timerCount(int tim){
+	
+	int i = 0;
+	
+	//Set Presaler TO 1
+	TCCR1B = (1<<CS10);
+	
+	//Set timer clear on Comparision CTC
+	TCCR1B |= (1<<WGM12);
+	
+	
+	
+	//Comparison Register to 1000 cycles for 1 ms
+	OCR1A = 0x03e8;		
+	
+	//Set inital Value of the timer Counter to 0x0000
+	TCNT1 = 0x0000;
+	
+	//Enable the output compare interrupt enable
+	TIMSK1 = TIMSK1|0x02;
+	
+	//Clear the timer interrupt flag and begin timer
+	TIFR1 = (1<<OCF1A);
+	
+	//Poll the timer to determine when the timer has reached 1ms
+	//Timer is set for 1 ms, so it will be in while for tim X 1ms.
+	while(i<tim){
+		if((TIFR1 & 0x02)==0x02){  // sees if the 0CF1A flag is up 
+			
+			TIFR1 =(1<<OCF1A);
+			i++;
+		}
+	}
+	return;
+}
