7984d8101cb59e479ed24c6640a8719a49be3acd
McM-Ev <mcmillan.ev@gmail.com>
2017-03-07 16:04:04 -0800

    Update usart10.c

--- TESTING/usart10.c
+++ TESTING/usart10.c
@@ -11,56 +11,59 @@ void timerCount(int tim);
 
 //PD3 = TX
 
-int main(){	
-
-  UBRROH = (BRC>>8);
-  UBRROL = BRC;
-  
-  UCSR0B = (1<<TXEN0);
-  UCSROC = (1<<UCSZ01)|(1<<UCSZ00);   //8bit
-  
-  while(1){
-    UDR0 = '8';
-    timerCount(1000);
-  }
-  
-
-  /**************************************************************************************/
-/***************************** Timer***************************************************/
-/**************************************************************************************/
-
-void timerCount(int tim){
+int main(){
+	DDRC=0xFF; 
+	UBRR1H = (BRC>>8);
+	UBRR1L = BRC;
 	
-	int i = 0;
+	UCSR1B = (1<<TXEN1);
+	UCSR1C = (1<<UCSZ11)|(1<<UCSZ10);   //8bit
 	
-	//Set Presaler TO 1
-	TCCR1B = (1<<CS10);
-	
-	//Set timer clear on Comparision CTC
-	TCCR1B |= (1<<WGM12);
-	
-	
-	
-	//Comparison Register to 1000 cycles for 1 ms
-	OCR1A = 0x03e8;		
-	
-	//Set inital Value of the timer Counter to 0x0000
-	TCNT1 = 0x0000;
-	
-	//Enable the output compare interrupt enable
-	TIMSK1 = TIMSK1|0x02;
-	
-	//Clear the timer interrupt flag and begin timer
-	TIFR1 = (1<<OCF1A);
-	
-	//Poll the timer to determine when the timer has reached 1ms
-	//Timer is set for 1 ms, so it will be in while for tim X 1ms.
-	while(i<tim){
-		if((TIFR1 & 0x02)==0x02){  // sees if the 0CF1A flag is up 
-			
-			TIFR1 =(1<<OCF1A);
-			i++;
-		}
+	while(1){
+		while ( !( UCSR1A & (1<<UDRE1)) )
+		PORTC = 8;
+		UDR1 = '8';
+		timerCount(2000);
 	}
-	return;
 }
+	
+
+	/**************************************************************************************/
+	/***************************** Timer***************************************************/
+	/**************************************************************************************/
+
+	void timerCount(int tim){
+		
+		int i = 0;
+		
+		//Set Presaler TO 1
+		TCCR1B = (1<<CS10);
+		
+		//Set timer clear on Comparision CTC
+		TCCR1B |= (1<<WGM12);
+		
+		
+		
+		//Comparison Register to 1000 cycles for 1 ms
+		OCR1A = 0x03e8;
+		
+		//Set inital Value of the timer Counter to 0x0000
+		TCNT1 = 0x0000;
+		
+		//Enable the output compare interrupt enable
+		TIMSK1 = TIMSK1|0x02;
+		
+		//Clear the timer interrupt flag and begin timer
+		TIFR1 = (1<<OCF1A);
+		
+		//Poll the timer to determine when the timer has reached 1ms
+		//Timer is set for 1 ms, so it will be in while for tim X 1ms.
+		while(i<tim){
+			if((TIFR1 & 0x02)==0x02){  // sees if the 0CF1A flag is up
+				
+				TIFR1 =(1<<OCF1A);
+				i++;
+			}
+		}
+		return;
+	}
