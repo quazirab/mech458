a473e08713cb816ea2e5e0b65f3cdbb8bb568ba1
quazirab <quazi.rabbi@gmail.com>
2017-03-18 05:06:06 -0700

    inserted interrupt

--- Final Code/Final Code.c
+++ Final Code/Final Code.c
@@ -22,9 +22,9 @@ const uint16_t Steel_min = 600;
 const uint16_t Black_min = 935;
 const uint16_t white_max =930;
 const uint16_t whie_min = 900;
+const uint8_t pwm = 160;
 
 typedef struct {
-	uint16_t  ADC_value; 	/* Stores for ADC Value */
 	uint8_t  pos;
 	uint8_t number;
 } part;
@@ -33,8 +33,6 @@ typedef struct cylinder{
 	part		p;
 	struct cylinder *next;
 } cylinder;
-
-
 
 
 void setupADC();
@@ -47,84 +45,78 @@ void cclockwise (int cclCount);
 void initcylinder( cylinder **newcylinder);
 void enqueue(cylinder **h, cylinder **t,cylinder **nL);
 void dequeue( cylinder **h, cylinder **t, cylinder **deQueuedcylinder);
+void stepperpos(uint8_t pos);
+void StepperHome();
 
 
 /*Usage of pins in this program*/
 //PORTC for LEDs
-//PF1 for ADC input 
-//PB7 for PWM output 
+//PF1 = RL 
+//PB7 = PWM 
 //PB5 for Direction Switch
-//PB0 TO EB
-//PB1 TO EA
-//PB2 TO IB
-//PB3 TO IA
+//PB0 = EB
+//PB1 = EA
+//PB2 = IB
+//PB3 = IA
 //PB0 = L4
 //PB1 = L4
 //PB2 = E2
 //PB3 = L2
 //PB4 = L1
 //PB5 = E1
+//PD0 = OI
+//PD1 = IN
+//PD2 = OR
+//PD3 = EX
 
-volatile uint8_t number = 0;
-volatile unsigned char ADClowerbit;
-volatile unsigned char ADChigherbit;						//Global variable for storing the current ADC conversion value
-volatile uint8_t Status_flag = 0;							//Global variable for the Status flag for newcylinder results found or not
 const char dcDrive[4] = {0x00,0x04,0x08,0x0c};				//Array containing the DC motor driver truth table
-volatile uint8_t STATE=0;
 volatile unsigned short lowVal1;
 volatile unsigned short lowVal2;							//short is 16 bit(2byte)
 volatile int countStep=0;									//Global variable to track the location of the stepper
 volatile int coilCount = 0;									//Global variable to track which coil was last used
+volatile int Status_flag = 0;								//Stepper operation only when not 0;
+volatile int Status_flag
 char step[4] = {0x36,0x2e,0x2d,0x35};						//Drives step 1&2, 2&3,3&4,4&1; dual phase full stepping
 cylinder *newcylinder =NULL;								//cylinder pointer for the new node to be added on each input
 cylinder *tail = NULL;										//cylinder pointer for the tail of the queue
 cylinder *head = NULL;										//cylinder pointer for the head of the queue
 cylinder *rtncylinder = NULL;								//cylinder pointer for returning the value of the node that has been dequeued
-void stepperpos(uint8_t pos);
-volatile uint8_t fallingStatus = 0;
+volatile uint8_t BELT_STATUS = 1;							//1-BELT MOVING, 0 FOR BELT STOP
+
 
 //volatile uint8_t pos = 0;								//value of the stepper position - 1 - white, 2 - black, 3 - steel , 4 - alu
 
 
 int main(){
 
-	DDRB = 0b10001111; 	//SET PORT B LOWER 4 BITS TO OUTPUT, WITH PB4,PB5,PB6 TO INPUT, AND PB7 TO OUTPUT
-	DDRC = 0xFF;;	//SET ALL OF THE PORT C TO OUTPUT BITS
+	DDRB = 0x0F; 	//SET PORT B LOWER 4 BITS TO OUTPUT, WITH PB4,PB5,PB6 TO INPUT, AND PB7 TO OUTPUT
+	DDRC = 0xFF;	//SET ALL OF THE PORT C TO OUTPUT BITS
 	DDRD = 0xF0;	//SET ALL OF THE PORT D TO OUTPUT BITS
 	DDRF = 0x00;	//SET ALL OF PORT F TO INPUT BITS
 	
 	
 	cli();				//Disable Global interrupts
-	setupADC();			//Run the ADC Setup Subroutine below
-	setupPWM();			//Run the PWM setup Subroutine below
+	setupADC();			//Run the ADC Setup Function below
+	setupPWM();			//Run the PWM setup Function below
 	setupInterrupt();	//Sets up all the interrupts
-	
 	sei();				//Enable global interrupts
-	
-	
-	
-	
+	StepperHome();		//brings the stepper to position
+
 	while(1){
 		
 	if ((PINB & 0x10) == 0x10){
-		PORTB = dcDrive[1];
-	}
-	if((PINB & 0x10) == 0x00){
-		
+		if (BELT_STATUS) PORTB = dcDrive[1];
+		else PORTB = 0;
+		}//if
+	else{
 		PORTB = 0;
-	}
-
-	if(fallingStatus)PORTB=0;
-	else PORTB = dcDrive[1];
-
-	if(Status_flag==1){
-		dequeue(&head,&tail,&rtncylinder);
-		stepperpos(rtncylinder->p.pos);
-		free(rtncylinder);
-		Status_flag=0;
-	}
-	
-			 
+		}//if
+	if(Status_flag){
+			dequeue(&head,&tail, &rtncylinder);
+			stepperpos(rtncylinder->p.pos);
+			Status_flag--;
+			if(Status_flag<0) Status_flag=0;
+		}//if	 
 	}//while
 	return(0);
 }/* main */
@@ -134,13 +126,12 @@ int main(){
 /*****************************Stepper Motor Clockwise**********************************/
 /**************************************************************************************/
 void stepperpos(uint8_t pos){
-	clockwise(100);	
+		clockwise(50*pos);
 }
 
 /**************************************************************************************/
 /*****************************Stepper Motor Clockwise**********************************/
 /**************************************************************************************/
-
 void clockwise(int clCount){
 	int delay = 20;
 	
@@ -160,7 +151,6 @@ void clockwise(int clCount){
 /**************************************************************************************/
 /*****************************Stepper Motor Counter Clockwise**************************/
 /**************************************************************************************/
-
 void cclockwise (int cclCount){
 	int delay = 20;
 	for(int i=0;i<cclCount;i++){
@@ -180,45 +170,37 @@ void cclockwise (int cclCount){
 /************************************Interrupt***********************************************/
 /**************************************************************************************/
 void setupInterrupt(){
-	EICRA |= _BV(ISC00);				//sets to any edges
-	EICRA |= _BV(ISC11);				//EX sensor triggers on falling edge
-	EICRA |= _BV(ISC31);				//First optical sensor triggers on 
-	EIMSK |= 0x09;						//needs to changed
-}
+	EICRA |= _BV(ISC00)|_BV(ISC01);		//OI - FOR INITIATING A LINK LIST - RISING EDGE
+	EICRA |= _BV(ISC10);				//OR - FOR ADC VALUE - ANY EDGE
+	EICRA |= _BV(ISC21);				//EX - STEPPER WAITER - FALLING EDGE
+	EICRA |= _BV(ISC31);				//HE - RESESTS THE STEPPER COUNT - FALLING EDGE
+	EIMSK |= 0x0F;						//needs to changed
+}//IN - FOR METAL DETECTION
 
-/*Interrupt 0: RL sensor on PD0*/
-ISR(INT0_vect){
-			
-	if(STATE==0){
-		STATE=1;						//changes the STATE to 1
-		lowVal2=0x03FF;					//initial highest vale to 1023
-		ADCSRA |= (1<<ADSC);			//start conversion, goes to ADC			
-		}//if
-	
-	else {
-		STATE=0;						//next edge
-	}//else
-	
+/*Interrupt 0: OI sensor on PD0*/
+ISR(INT0_vect){											//OI 
+	/*Initialize new list element when the first optical sensor is triggered.*/
+// 		initcylinder(&newcylinder);
+// 		newcylinder->p.number = number;				//puts the position number of the cylinder in the newcylinder
+// 		enqueue(&head,&tail, &newcylinder);			//enqueues the new cylinder
+// 		number++;
 }//ISR
 
-/*Interrupt 1: inductive sensor on PD1*/
+/*Interrupt 1: OR on PD1*/
 ISR(INT1_vect){
-	/*Record if it is a metal. TRUE/FALSE*/
-	
-		PORTC = 0xFF;
-
-
-		
-	
+	/*ADC VALUE*/
+	lowVal2=0x03FF;							//intial highest vale to 1023
+	ADCSRA |= (1<<ADSC);					//start conversion, goes to ADC	
 }//ISR
-
+/*Interrupt 1: OR on PD1*/
+ISR (INT2_vect){
+	/*WAIT AT THE END OF THE BELT*/
+	if (BELT_STATUS==1)BELT_STATUS=0;		//stops the belt if the 
+	else BELT_STATUS =1;
+}
 /*Interrupt 3: First optical sensor on PD3*/
 ISR(INT3_vect){
-	/*Initialize new list element when the first optical sensor is triggered.*/
-	initcylinder(&newcylinder);
-	newcylinder->p.number = number;				//puts the position number of the cylinder in the newcylinder
-	enqueue(&head,&tail, &newcylinder);			//enqueues the new cylinder
-	number++;
+	
 }//ISR
 
 
@@ -236,12 +218,6 @@ void setupPWM(){
 }//setupPWM
 
 
-/*Subroutine to brake to VCC for 'tim' milliseconds. New motor direction is set in the main code after this is called.*/
-void brakeVcc(int tim){
-	PORTB  = dcDrive[0];	//Brake to VCC before changing direction
-	timerCount(tim);		//Delay 
-}
-
 /**************************************************************************************/
 /*************************************ADC**********************************************/
 /**************************************************************************************/
@@ -252,45 +228,32 @@ void setupADC(){
 }/*setupADC*/
 
 ISR(ADC_vect){
-	lowVal1 = ADC;			//puts the full 10 bit value to lowVal1	
-	if(lowVal2>lowVal1){		//if lowVal2 is greater than lowVal1
-		lowVal2=lowVal1;	//lowVal2 is equal to lowVal1 
+	lowVal1 = ADC;										//puts the full 10 bit value to lowVal1
+	if(lowVal2>lowVal1){								//if lowVal2 is greater than lowVal1
+		lowVal2=lowVal1;								//lowVal2 is equal to lowVal1
 	}//if
-	if(STATE==1) ADCSRA |= (1<<ADSC);        //Set the ADC Start Conversion bit on ADCSRA to 1.;	//conversion continues till state ==0;
-	else {
-		if (head->p.ADC_value==NULL) {		//checks if the head has any ADC value or not
-			head->p.ADC_value=lowVal2;
-			//PORTC = head->p.ADC_value;
-			if (head->p.ADC_value<Al_max){//aluminum
-					head->p.pos=4;	
-		 
+	if((PIND & 0x01) == 0x01) ADCSRA |= (1<<ADSC);			//conversion continues till state ==0;
+	else{
+		initcylinder(&newcylinder);							//Initialize the newcylinder
+			/*1 - white, 2 - black, 3 - steel , 4 - alu*/
+			if (lowVal2<Al_max){							//aluminum
+					newcylinder->p.pos=4;	
 				}//if
-			else if (head->p.ADC_value>Steel_min && head->p.ADC_value<Steel_max){//aluminum
-				head->p.pos=3;
-				
+			else if (lowVal2>Steel_min && lowVal2<Steel_max){//STEEL
+					newcylinder->p.pos=3;
 				}//if
-			else if (head->p.ADC_value>Black_min){//aluminum
-				head->p.pos=2;
+			else if (lowVal2>Black_min){					//BLACK
+					newcylinder->p.pos=2;
 				}//if
 			
-			else if (head->p.ADC_value<white_max&&head->p.ADC_value>whie_min){//aluminum
-				head->p.pos=1;
+			else if (lowVal2<white_max && lowVal2>whie_min){//WHITE
+					newcylinder->p.pos=1;
 				}//if
-			/*PORTC = head->p.pos;*/	
-			Status_flag =1;
-			return;
+			else PORTC = 0XFF;	
+			enqueue(&head,&tail, &newcylinder);				//enqueues the new cylinder
+			Status_flag ++;								// 1 - turns on stepper
 		}//if
-// 		else if (head->next->ADC_value=NULL){	//if the head has any ADC value, checks the next one
-// 			head->next->next->ADC_value=lowVal2; //assigns the ADC value to the next null one
-// 			
-// 		}//if
-// 		else if (head->next->next->ADC_value=NULL){	//if the head has any ADC value, checks the next one
-// 			head->next->next->ADC_value=lowVal2;	//assigns the ADC value to the next null one
-// 			
-// 		}//if
-// 		else if (head->next->next->next->ADC_value=NULL){	//if the head has any ADC value, checks the next one
-// 			head->next->next->next->next->ADC_value=lowVal2;	//assigns the ADC value to the next null one
-// 		}//if
+
 	}//else
 }//ISR
 
@@ -299,10 +262,6 @@ ISR(ADC_vect){
 /*************************************LinkList*********************************************/
 /**************************************************************************************/
 
-/**************************************************************************************
-* DESC: This initializes a cylinder and returns the pointer to the new cylinder or NULL if error 
-* INPUT: the head and tail pointers by reference
-*/
 void initcylinder( cylinder **newcylinder){
 	//cylinder *l;
 	*newcylinder = malloc(sizeof(cylinder));
@@ -310,41 +269,19 @@ void initcylinder( cylinder **newcylinder){
 	return;
 }/*initcylinder*/
 
-
-
-/****************************************************************************************
-*  DESC: Accepts as input a new cylinder by reference, and assigns the head and tail		
-*  of the queue accordingly				
-*  INPUT: the head and tail pointers, and a pointer to the new cylinder that was created 
-*/
-/* will put an item at the tail of the queue */
 void enqueue( cylinder **h, cylinder **t, cylinder **nL){
 
 	if (*t != NULL){
-		/* Not an empty queue */
 		(*t)->next = *nL;
-		*t = *nL; //(*t)->next;
+		*t = *nL; 
 	}/*if*/
 	else{
-		/* It's an empty Queue */
-		//(*h)->next = *nL;
-		//should be this
-		*h = *nL;
+	
 		*t = *nL;
 	}/* else */
 	return;
 }/*enqueue*/
 
-
-
-
-
-/**************************************************************************************
-* DESC : Removes the cylinder from the head of the list and assigns it to deQueuedcylinder
-* INPUT: The head and tail pointers, and a ptr 'deQueuedcylinder' 
-* 		 which the removed cylinder will be assigned to
-*/
-/* This will remove the cylinder and element within the cylinder from the head of the queue */
 void dequeue( cylinder **h, cylinder **t, cylinder **deQueuedcylinder){
 	/* ENTER YOUR CODE HERE */
 	*deQueuedcylinder = *h;	// Will set to NULL if Head points to NULL
@@ -370,8 +307,6 @@ void timerCount(int tim){
 	
 	//Set timer clear on Comparision CTC
 	TCCR1B |= (1<<WGM12);
-	
-	
 	
 	//Comparison Register to 1000 cycles for 1 ms
 	OCR1A = 0x03e8;
